a05e35655f4080b8d1bad222611d4e43cb743c14
Lamarssom <ggbarabe2@gmail.com>
2026-02-18 14:21:15 +0100

    Neon db update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,17 +10,23 @@ import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ 
-      isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT ?? '5432', 10),
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'Biology1999web',
-      database: process.env.DB_NAME || 'fx_app',
+      url: process.env.DATABASE_URL || process.env.POSTGRES_URL,
+      //host: process.env.DB_HOST || 'localhost',
+      //port: parseInt(process.env.DB_PORT ?? '5432', 10),
+      //username: process.env.DB_USERNAME || 'postgres',
+      //password: process.env.DB_PASSWORD || 'Biology1999web',
+      //database: process.env.DB_NAME || 'fx_app',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      logging: true,
     }),
     AuthModule,
     WalletModule,
